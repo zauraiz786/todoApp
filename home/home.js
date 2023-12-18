c701d7cc6fd7b338859d9fcfa8e49628966023f8
zauraiz786 <zauraizhassankhan99@gmail.com>
2023-12-18 12:49:59 +0500

    updated

--- home/home.js
+++ home/home.js
@@ -1,8 +1,6 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"; 
-import { auth } from "../config.js";
-import { db } from "../config.js";
+import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { auth, db } from "../config.js";
 
 //Check User is login or not
 onAuthStateChanged(auth, (user) => {
@@ -18,6 +16,7 @@ const button = document.querySelector('#button');
 const add = document.querySelector('.add');
 const input = document.getElementById('myInput');
 const form = document.getElementById('form');
+const ul = document.getElementById('ul');
 
 button.addEventListener('click', () => {
   signOut(auth).then(() => {
@@ -27,7 +26,17 @@ button.addEventListener('click', () => {
   })
 })
 
-form.addEventListener('submit',async (event)=>{
+add.addEventListener('click', () => {
+  let li = document.createElement('li');
+  li.classList.add('font');
+  let t = document.createTextNode(input.value);
+  li.prepend(t)
+  if (input.value === '') alert('Please enter text')
+  else ul.prepend(li);
+})
+
+//send data
+form.addEventListener('submit', async (event) => {
   event.preventDefault()
   try {
     const docRef = await addDoc(collection(db, "todos"), {
@@ -40,11 +49,21 @@ form.addEventListener('submit',async (event)=>{
   }
 })
 
-add.addEventListener('click',function(){
-  let li = document.createElement('li');
-  let t = document.createTextNode(input.value);
-  li.prepend(t)
-  if (input.value === '') alert('Please enter text')
-  else document.getElementById('ul').prepend(li);
-  input.value = '';
-})
+//render data
+let arr = [];
+async function renderTodo() {
+  const querySnapshot = await getDocs(collection(db, "todos"));
+  querySnapshot.forEach((doc) => {
+    arr.push(doc.data());
+  });
+  console.log(arr);
+  arr.map((item) => {
+    let li = document.createElement('li');
+    li.classList.add('font');
+    let t = document.createTextNode(item.title);
+    li.prepend(t)
+    ul.prepend(li);
+    // console.log(item.title);
+  })
+}
+renderTodo()
